4618fcd4b6abbf6e58af83e3e96b6884fe901064
vicky-wills <vigneshofficial2017@gmail.com>
2023-06-28 15:41:23 +0530

    Animation

---
+++ assets/js/Scroll-reveal.js
@@ -0,0 +1,45 @@
+//Header//
+ScrollReveal().reveal('.header-logo a', { distance: '0px', duration: 200, origin: 'center', delay: 300, scale: 0.8, opacity: 0, easing: 'ease-out' });
+ScrollReveal().reveal('.header-logo .left', { distance: '400px', duration: 400, origin: 'right', delay: 0, scale: 0.5, opacity: 0, easing: 'ease-out' });
+ScrollReveal().reveal('.header-logo .right', { distance: '400px', duration: 400, origin: 'left', delay: 0, scale: 0.5, opacity: 0, easing: 'ease-out' });
+
+
+//Hero//
+ScrollReveal().reveal('.banner-text h2', { distance: '200px', duration: 500, origin: 'top', delay: 0, opacity: 0, easing: 'ease-out' });
+
+ScrollReveal().reveal('.hero-text-1', { distance: '400px', duration: 500, origin: 'right', delay: 50, opacity: 0, easing: 'ease-out' });
+ScrollReveal().reveal('.hero-text-2', { distance: '400px', duration: 500, origin: 'right', delay: 150, opacity: 0, easing: 'ease-out' });
+ScrollReveal().reveal('.hero-text-3', { distance: '400px', duration: 500, origin: 'right', delay: 250, opacity: 0, easing: 'ease-out' });
+
+ScrollReveal().reveal('.hero-text-1 span', { distance: '400px', duration: 600, origin: 'right', delay: 100, interval: 50, opacity: 0, easing: 'ease-out' });
+ScrollReveal().reveal('.hero-text-2 span', { distance: '400px', duration: 600, origin: 'right', delay: 200, interval: 50, opacity: 0, easing: 'ease-out' });
+ScrollReveal().reveal('.hero-text-3 span', { distance: '400px', duration: 600, origin: 'right', delay: 300, interval: 50, opacity: 0, easing: 'ease-out' });
+
+
+//Headings//
+ScrollReveal().reveal('.site-heading h4', { distance: '50px', duration: 400, origin: 'top', delay: 20, opacity: 0, easing: 'ease-out' });
+ScrollReveal().reveal('.site-heading h2', { distance: '50px', duration: 400, origin: 'bottom', delay: 20, opacity: 0, easing: 'ease-out' });
+
+
+//Our Services//
+ScrollReveal().reveal('.work-process-items .top-animate .single-item', { distance: '50px', duration: 400, origin: 'bottom', delay: 100, interval: 100, opacity: 0, easing: 'ease-out' });
+ScrollReveal().reveal('.work-process-items .bottom-animate .single-item', { distance: '50px', duration: 400, origin: 'bottom', delay: 200, interval: 100, opacity: 0, easing: 'ease-out' });
+
+
+//About//
+ScrollReveal().reveal('.about-area .container .accordion .card', { distance: '50px', duration: 400, origin: 'right', delay: 50, interval: 50, easing: 'ease-out' });
+
+
+//FAQ//
+ScrollReveal().reveal('.faq-area .faq-content .accordion .card', { distance: '50px', duration: 400, origin: 'right', delay: 50, interval: 50, easing: 'ease-out' });
+
+
+//Footer//
+ScrollReveal().reveal('footer .footer-top-container .footer-top div', { distance: '100px', duration: 400, origin: 'top', delay: 0, interval: 50, easing: 'ease-out' });
+ScrollReveal().reveal('footer .footer-contact-container .foo-address', { distance: '100px', duration: 400, origin: 'right', delay: 100, interval: 100, easing: 'ease-out' });
+
+
+
+
+
+
